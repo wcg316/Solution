d881180adb65cb89d0307dabdfb5d1b8529c1186
wcg316 <henry.rem.rem@gmail.com>
2025-06-08 22:41:00 +0800

    未完成 2024/6 4.最佳選擇

---
+++ ZeroJudge/APCS/2024_06_4.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define OFFSET 2001
+//#define int long long
+//#define getchar getchar_unlocked  
+//#define putchar putchar_unlocked
+using namespace std;
+
+inline int read() {
+    char ch = getchar();
+    while (ch == ' ' || ch == '\n') ch = getchar();
+    if (ch == EOF) return EOF;
+    int s = 1;
+    while (ch < '0' || '9' < ch) {
+        if (ch == '-') s = -1;
+        ch = getchar();
+    }
+    int r = 0;
+    while ('0' <= ch && ch <= '9') r = r * 10 + ch - '0', ch = getchar();
+    return r * s;
+}
+
+void write(int x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+struct GapAndSum {
+	int parityGap = 0, sum = 0;
+};
+
+signed main() {
+    int n = read(), k = read();
+	vector<int> a(n + 1);
+    vector<GapAndSum> prefix(n + 2), suffix(n + 2);
+	vector<vector<pair<int, int>>> gapMap(4005);
+	for (int i = 1; i <= n; i++) {
+		a[i] = read();
+		prefix[i].sum = prefix[i - 1].sum + a[i];
+		prefix[i].parityGap = prefix[i - 1].parityGap + (a[i] & 1 ? 1 : -1);
+	}
+	for (int i = n; i >= 1; i--) {
+		suffix[i].sum = suffix[i + 1].sum + a[i];
+		suffix[i].parityGap = suffix[i + 1].parityGap + (a[i] & 1 ? 1 : -1);
+		gapMap[suffix[i].parityGap + OFFSET].push_back({i, suffix[i].sum});
+	}
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		int neededGap = -prefix[i].parityGap;
+		
+	}
+	write(ans);
+	return 0;
+}
