c4498b0ece207e115631550fac0dce74659bde51
wcg316 <henry.rem.rem@gmail.com>
2026-09-04 13:10:48 +0800

    solved 2246A

---
+++ Codeforces/2246A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+//#define int long long
+//#define getchar getchar_unlocked
+//#define putchar putchar_unlocked
+using namespace std;
+
+inline int read() {
+    char ch = getchar();
+    while (ch == ' ' || ch == '\n') ch = getchar();
+    if (ch == EOF) return EOF;
+    int s = 1;
+    while (ch < '0' || '9' < ch) {
+        if (ch == '-') s = -1;
+        ch = getchar();
+    }
+    int r = 0;
+    while ('0' <= ch && ch <= '9') r = r * 10 + ch - '0', ch = getchar();
+    return r * s;
+}
+
+void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+signed main() {
+    int t = read();
+	while (t--) {
+		int n = read();
+		for (int i = 2; i <= n; i += 2) {
+			write(i);
+			putchar(' ');
+			write(i - 1);
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+	return 0;
+}
